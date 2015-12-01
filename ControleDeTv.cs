17395333224450560e697424b1ee45b3d0e3a19a
Ismael N <ismael.ismaio@gmail.com>
2015-12-01 10:34:00 -0300

    Create ControleDeTv.cs

---
+++ ControleDeTv.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class teste : MonoBehaviour
+{
+	private int 	iNumero;
+	public Button 	btn;
+	public Text 	tSaida;
+
+	//0 = Desliga.
+	//1 = Ligada.
+	void Start()
+	{
+		//Valor inicial da variavel(Ligada).
+		iNumero = 1;
+	}
+	void Update()
+	{
+		Debug.Log (iNumero);
+	}
+
+	public void LigarDesliga()
+	{
+		//Valor da variavel(deligada).
+		iNumero--;
+
+		//Trocando valor da variavel( iNumero ).
+		if (iNumero < 0)
+		{
+			iNumero = iNumero + 2;
+		}
+
+		Verificando ();
+	}
+
+	public void Verificando()
+	{
+		if( iNumero == 1 )
+		{
+			tSaida.GetComponent<Text>().text = "Ligado";
+		}
+
+		if( iNumero == 0 )
+		{
+			tSaida.GetComponent<Text>().text = "Desligado";
+		}
+
+
+	}
+
+
+
+
+}
